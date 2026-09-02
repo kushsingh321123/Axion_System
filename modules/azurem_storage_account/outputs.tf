fe542d2724b786a0b2af8045d64e9eb1294966a2
kushsingh321123 <kushsingh321123>
2026-09-03 03:36:14 +0530

    update

--- modules/azurem_storage_account/outputs.tf
+++ modules/azurem_storage_account/outputs.tf
@@ -1,3 +1,9 @@
 output "storage_accounts" {
-  value = azurerm_storage_account.storage
+  value = {
+    for key, storage in azurerm_storage_account.storage : key => {
+      name                = storage.name
+      resource_group_name = storage.resource_group_name
+      location            = storage.location
+    }
+  }
 }

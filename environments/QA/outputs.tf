fbc576a02a5803be1277f3ffdf4bb0a2cd5a18bb
kushsingh321123 <kushsingh321123>
2026-09-03 02:27:15 +0530

    add

---
+++ environments/QA/outputs.tf
@@ -0,0 +1,7 @@
+output "resource_groups" {
+  value = module.rg.resource_groups
+}
+
+output "storage_accounts" {
+  value = module.storage.storage_accounts
+}
--- environments/QA/variables.tf
+++ environments/QA/variables.tf
@@ -4,3 +4,11 @@ variable "resource_groups" {
     rg_location = string
   }))
 }
+variable "storage_accounts" {
+  type = map(object({
+    name                     = string
+    resource_group_key       = string
+    account_tier             = string
+    account_replication_type = string
+  }))
+}
